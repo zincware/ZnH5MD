44a02403884f60126ad9f8f86f98f5fe4da3dc92
PythonFZ <fabian.zills@web.de>
2024-07-17 16:00:25 +0200

    add `znh5md.__version__`

--- tests/test_znh5md.py
+++ tests/test_znh5md.py
@@ -1,13 +1,4 @@
-# import ase.collections
-# import znh5md
-# import numpy.testing as npt
+import znh5md
 
-# def test_write_read(tmp_path):
-#     structures = list(ase.collections.s22)
-#     znh5md.write(tmp_path / 'test.h5', structures)
-#     structures2 = znh5md.read(tmp_path / 'test.h5')
-
-#     assert len(structures) == len(structures2)
-#     for a, b in zip(structures, structures2):
-#         npt.assert_array_equal(a.get_positions(), b.get_positions())
-#         npt.assert_array_equal(a.get_atomic_numbers(), b.get_atomic_numbers())
+def test_version():
+    assert znh5md.__version__ == "0.3.0"
--- znh5md/__init__.py
+++ znh5md/__init__.py
@@ -1,4 +1,7 @@
 from .io import IO
 from .znh5md import iread, read, write
+import importlib.metadata
 
 __all__ = ["IO", "read", "write", "iread"]
+
+__version__ = importlib.metadata.version("znh5md")

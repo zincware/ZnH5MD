50fa7cea9e2c485cf21f6c9c0c13d4d0f0725bc2
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2024-07-26 11:18:34 +0200

    Update test_read_write.py

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -33,6 +33,7 @@ def test_iread(tmp_path, s22_all_properties):
     znh5md.write(file, s22_all_properties)
     
     assert len(list(znh5md.iread(file))) == len(s22_all_properties)
+    assert len(list(znh5md.IO(file))) == len(s22_all_properties)
 
     for a, b in zip(s22_all_properties, znh5md.iread(file)):
         npt.assert_array_equal(a.get_positions(), b.get_positions())

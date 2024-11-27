7e15a117875120c094c2eb87be6e1d84701e17bb
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-27 10:13:57 +0100

    move to `extend` function

--- tests/serialization/test_serialisation.py
+++ tests/serialization/test_serialisation.py
@@ -47,6 +47,47 @@ def test_frames_iter(dataset_name, request):
                 npt.assert_array_equal(a.calc.results[key], b.calc.results[key])
 
 
+@pytest.mark.parametrize(
+    "dataset_name",
+    [
+        "s22",
+        "s22_energy",
+        "s22_energy_forces",
+        "s22_all_properties",
+        "s22_info_arrays_calc",
+        "s22_mixed_pbc_cell",
+        "s22_illegal_calc_results",
+        "s22_no_ascii",
+        "frames_with_residuenames",
+    ],
+)    
+def test_frames_getitem(dataset_name, request):
+    dataset = request.getfixturevalue(dataset_name)
+    frames = zns.encode(dataset)
+
+    for idx, frame in enumerate(dataset):
+        a = frames[idx]
+        b = frame
+
+        npt.assert_array_equal(a.positions, b.positions)
+        npt.assert_array_equal(a.cell, b.cell)
+        npt.assert_array_equal(a.pbc, b.pbc)
+        npt.assert_array_equal(a.numbers, b.numbers)
+
+        for key in b.arrays:
+            npt.assert_array_equal(a.arrays[key], b.arrays[key])
+        for key in b.info:
+            npt.assert_array_equal(a.info[key], b.info[key])
+        if b.calc is not None:
+            for key in b.calc.results:
+                npt.assert_array_equal(a.calc.results[key], b.calc.results[key])
+
+
+def test_len(s22):
+    frames = zns.encode(s22)
+
+    assert len(frames) == 22
+
 def test_serialisation_s22_energy(s22_energy):
     frames = zns.encode(s22_energy)
 
--- znh5md/serialization/abc.py
+++ znh5md/serialization/abc.py
@@ -1,49 +1,145 @@
 import dataclasses
-
 import numpy as np
+import numpy.typing as nptype
 import ase
 from ase.calculators.singlepoint import SinglePointCalculator
 import typing as t
 
-# NumPy does not yet support type hints with shapes
-# https://github.com/numpy/numpy/issues/16544
+CONTENT_TYPE = dict[str, np.ndarray | dict | float | int | str | bool]
 
 
-# TODO: convert this into a mutable sequence?
+def process_category(target: dict[str, list], content: CONTENT_TYPE, index) -> None:
+    """
+    Process a category (arrays, info, calc) for a single atoms object, ensuring
+    that keys are added and missing values are backfilled.
+
+    Parameters:
+        target (dict): The main dictionary storing data for the category.
+        atoms_data (dict): The data from the current atoms object (arrays, info, or calc).
+        index (int): The index of the current atoms object in the trajectory used for backfilling.
+    """
+
+    seen = set(content.keys())
+    unseen = set(target.keys()) - seen
+
+    for key in content:
+        if key not in target:
+            # Backfill existing entries with MISSING for the new key
+            target[key] = [MISSING] * index + [content[key]]
+        else:
+            # Add the new data to the existing key
+            target[key].append(content[key])
+
+    for key in unseen:
+        # Backfill missing entries with MISSING for the unseen key
+        target[key].append(MISSING)
+
 
 @dataclasses.dataclass
 class Frames:
     """Dataclass for Atoms object serialization."""
 
-    positions: np.ndarray  # (N, )
-    numbers: np.ndarray  # (N, )
-    pbc: np.ndarray  # (N, 3)
-    cell: np.ndarray  # (N, 3, 3)
-    arrays: dict[str, np.ndarray]  # (N, ...), where ... is the shape of the array
-    info: dict[str, np.ndarray]  # (N, ...), where ... is the shape of the array
-    calc: dict[str, np.ndarray]  # (N, ...), where ... is the shape of the array
-
+    positions: nptype.NDArray[np.object_] | None = None  # (N, )
+    numbers: nptype.NDArray[np.object_] | None = None  # (N, )
+    pbc: nptype.NDArray[np.bool_] | None = None  # (N, 3)
+    cell: nptype.NDArray[np.float_] | None = None  # (N, 3, 3)
+    arrays: dict[str, nptype.NDArray[np.object_]] = dataclasses.field(
+        default_factory=dict
+    )  # (N, m, ...) where m is the number of atoms in the frame
+    info: dict[str, nptype.NDArray[np.object_]] = dataclasses.field(
+        default_factory=dict
+    )  # (N, ...)
+    calc: dict[str, nptype.NDArray[np.object_]] = dataclasses.field(
+        default_factory=dict
+    )  # (N, ...)
 
     def __iter__(self) -> t.Iterator[ase.Atoms]:
         """Iterate over the frames."""
+        if self.positions is None:
+            return iter([])
         for idx in range(len(self.positions)):
-            atoms = ase.Atoms(numbers=self.numbers[idx], positions=self.positions[idx], cell=self.cell[idx], pbc=self.pbc[idx])
-            # TODO: remove MISSING values!
-            for key in self.arrays:
-                if self.arrays[key][idx] is not MISSING:
-                    atoms.arrays[key] = self.arrays[key][idx]
-            for key in self.info:
-                if self.info[key][idx] is not MISSING:
-                    atoms.info[key] = self.info[key][idx]
-            for key in self.calc:
-                if self.calc[key][idx] is not MISSING:
-                    if atoms.calc is None:
-                        atoms.calc = SinglePointCalculator(atoms)
-                    atoms.calc.results[key] = self.calc[key][idx]
-            yield atoms
+            yield self[idx]
+
+    def __len__(self) -> int:
+        """Return the number of frames."""
+        if self.positions is None:
+            return 0
+        return len(self.positions)
+
+    def __getitem__(self, idx: int) -> ase.Atoms:
+        """Return a single frame."""
+        if self.positions is None:
+            raise IndexError("No frames in Frames object.")
+        atoms = ase.Atoms(
+            numbers=self.numbers[idx],
+            positions=self.positions[idx],
+            cell=self.cell[idx],
+            pbc=self.pbc[idx],
+        )
+        for key in self.arrays:
+            if not isinstance(self.arrays[key][idx], _MISSING):
+                atoms.arrays[key] = self.arrays[key][idx]
+        for key in self.info:
+            if not isinstance(self.info[key][idx], _MISSING):
+                atoms.info[key] = self.info[key][idx]
+        for key in self.calc:
+            if not isinstance(self.calc[key][idx], _MISSING):
+                if atoms.calc is None:
+                    atoms.calc = SinglePointCalculator(atoms)
+                atoms.calc.results[key] = self.calc[key][idx]
+
+        return atoms
+
+    def append(self, atoms: ase.Atoms) -> None:
+        """Append a frame to the frames."""
+        self.extend([atoms])
+
+    def extend(self, frames: t.Iterable[ase.Atoms]) -> None:
+        """Extend the frames with a sequence of frames."""
+        positions = np.array([atoms.positions for atoms in frames], dtype=object)
+        numbers = np.array([atoms.numbers for atoms in frames], dtype=object)
+        pbc = np.array([atoms.pbc for atoms in frames])
+        cell = np.array([atoms.cell.array for atoms in frames])
+
+        if self.positions is None:
+            self.positions = positions
+            self.numbers = numbers
+            self.pbc = pbc
+            self.cell = cell
+        else:
+            raise NotImplementedError("Can only extend Frames once.")
+            self.positions = np.concatenate([self.positions, positions])
+            self.numbers = np.concatenate([self.numbers, numbers])
+            self.pbc = np.concatenate([self.pbc, pbc])
+            self.cell = np.concatenate([self.cell, cell])
+
+        for idx, atoms in enumerate(frames):
+            # Process arrays
+            atoms_arrays = {
+                key: value
+                for key, value in atoms.arrays.items()
+                if key not in ["positions", "numbers"]
+            }
+            process_category(self.arrays, atoms_arrays, idx)
+
+            # Process info
+            process_category(self.info, atoms.info, idx)
+
+            # Process calc
+            if atoms.calc is not None:
+                process_category(self.calc, atoms.calc.results, idx)
+
+        for key in self.arrays:
+            self.arrays[key] = np.array(self.arrays[key], dtype=object)
+        for key in self.info:
+            self.info[key] = np.array(self.info[key], dtype=object)
+        for key in self.calc:
+            self.calc[key] = np.array(self.calc[key], dtype=object)
 
 
 class _MISSING:
+    """Sentinel value for missing entries."""
+
     pass
 
 

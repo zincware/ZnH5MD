091a97b6e17674d71c3c68023caf76c81d60b187
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-05-23 08:48:05 +0200

    update scale

--- benchmarks/read.py
+++ benchmarks/read.py
@@ -179,14 +179,14 @@ def plot_benchmarks(df_avg, df_std_avg, format: str):
 
 
 def main():
-    num_atoms = 16
+    num_atoms = 512
     for format in ["xtc", "h5md", "xyz", "pdb", "h5md-uncompressed"]:
         print(f"Running benchmark for {format.upper()} format")
         full_results = {
             num_frames: benchmark_io_for_frame_count(
                 num_atoms=num_atoms, num_frames=num_frames, file_format=format
             )
-            for num_frames in tqdm(np.logspace(1, 2, num=10, dtype=int))
+            for num_frames in tqdm(np.logspace(2, 3, num=10, dtype=int))
         }
 
         df_avg, df_std_avg = compute_benchmark_dfs(full_results, num_atoms=num_atoms)
